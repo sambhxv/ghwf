533c98b02cd9a3d3c76a257eb354d7c6e2aa05d0
Sambhav Saxena <sambhav.saxena@ambibuzz.com>
2025-02-11 21:32:07 +0530

    skdjfns
    Signed-off-by: Sambhav Saxena <sambhav.saxena@ambibuzz.com>

--- github_workflows/github_workflows/doctype/temperature/temperature.py
+++ github_workflows/github_workflows/doctype/temperature/temperature.py
@@ -5,9 +5,9 @@
 from frappe.model.document import Document
 import frappe
 
-def wieodwn():
+def uwyerfjas():
     request = "lololmao"
     frappe.msgprint(request)
 
 class Temperature(Document):
-	wieodwn()
+	uwyerfjas()
