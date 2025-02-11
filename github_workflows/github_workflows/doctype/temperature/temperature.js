eeb1197b2b601e8e75c9b40620737ca4102b6c99
Sambhav Saxena <sambhav.saxena@ambibuzz.com>
2025-02-11 23:39:10 +0530

    hehe
    Signed-off-by: Sambhav Saxena <sambhav.saxena@ambibuzz.com>

--- github_workflows/github_workflows/doctype/temperature/temperature.js
+++ github_workflows/github_workflows/doctype/temperature/temperature.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2025, Ambibuzz Technologies LLP and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Temperature", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Temperature", {
+	refresh(frm) {
+        console.log(frm)
+	},
+});
