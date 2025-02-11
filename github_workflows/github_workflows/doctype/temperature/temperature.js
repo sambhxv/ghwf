0147eaf1ba4e2848a4618af1593090f89c624eed
Sambhav Saxena <sambhav.saxena@ambibuzz.com>
2025-02-11 20:44:25 +0530

    added initial workflow

---
+++ github_workflows/github_workflows/doctype/temperature/temperature.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Ambibuzz Technologies LLP and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Temperature", {
+// 	refresh(frm) {
+
+// 	},
+// });
