88efbc8703ca102e75e0ffa4eb34ca4f7a53dc8e
Sambhav Saxena <sambhav.saxena@ambibuzz.com>
2025-02-12 18:51:42 +0530

    created track record doc
    Signed-off-by: Sambhav Saxena <sambhav.saxena@ambibuzz.com>

--- github_workflows/github_workflows/doctype/track_record/track_record.js
+++ github_workflows/github_workflows/doctype/track_record/track_record.js
@@ -2,10 +2,10 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Track Record", {
-    units: function (frm) {
-        let units = parseInt(frm.doc.units);
-        if (!isNaN(units)) {
-            frm.set_value("duration", units * 2);
-        }
-    }
+	units: function (frm) {
+		let units = parseInt(frm.doc.units);
+		if (!isNaN(units)) {
+			frm.set_value("duration", units * 2);
+		}
+	}
 });
--- github_workflows/github_workflows/doctype/track_record/track_record.py
+++ github_workflows/github_workflows/doctype/track_record/track_record.py
@@ -9,8 +9,8 @@ class TrackRecord(Document):
 	pass
 
 def update_duration(docname):
-    doc = frappe.get_doc("Track Record", docname)
-    if doc.units and doc.units.isdigit():
-        doc.duration = int(doc.units) * 2
-        doc.save()
-        frappe.db.commit()
+	doc = frappe.get_doc("Track Record", docname)
+	if doc.units and doc.units.isdigit():
+		doc.duration = int(doc.units) * 2
+		doc.save()
+		frappe.db.commit()
