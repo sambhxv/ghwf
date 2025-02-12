0807105a7f0b32bee3460c318726e53b0c2dd1c2
Sambhav Saxena <sambhav.saxena@ambibuzz.com>
2025-02-12 18:23:08 +0530

    created track record doc
    Signed-off-by: Sambhav Saxena <sambhav.saxena@ambibuzz.com>

--- github_workflows/github_workflows/doctype/track_record/track_record.js
+++ github_workflows/github_workflows/doctype/track_record/track_record.js
@@ -1,8 +1,11 @@
 // Copyright (c) 2025, Ambibuzz Technologies LLP and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Track Record", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Track Record", {
+    units: function (frm) {
+        let units = parseInt(frm.doc.units);
+        if (!isNaN(units)) {
+            frm.set_value("duration", units * 2);
+        }
+    }
+});
--- github_workflows/github_workflows/doctype/track_record/track_record.py
+++ github_workflows/github_workflows/doctype/track_record/track_record.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2025, Ambibuzz Technologies LLP and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class TrackRecord(Document):
 	pass
+
+def update_duration(docname):
+    doc = frappe.get_doc("Track Record", docname)
+    if doc.units and doc.units.isdigit():
+        doc.duration = int(doc.units) * 2
+        doc.save()
+        frappe.db.commit()
