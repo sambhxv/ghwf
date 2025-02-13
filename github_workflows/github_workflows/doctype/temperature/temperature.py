d8cf58630807ff33c4bf864cab22cd6f05198871
Sambhav Saxena <sambhav.saxena@ambibuzz.com>
2025-02-13 19:19:18 +0530

    Update temperature.py

--- github_workflows/github_workflows/doctype/temperature/temperature.py
+++ github_workflows/github_workflows/doctype/temperature/temperature.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2025, Ambibuzz Technologies LLP and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Temperature(Document):
-	pass
+	frappe.msgprint(Document)
