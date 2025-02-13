a2cb35640871c6f89c2c7f786f61cb82ac43292b
Sambhav Saxena <sambhav.saxena@ambibuzz.com>
2025-02-13 19:27:31 +0530

    Update temperature.js

--- github_workflows/github_workflows/doctype/temperature/temperature.js
+++ github_workflows/github_workflows/doctype/temperature/temperature.js
@@ -6,3 +6,60 @@ frappe.ui.form.on("Temperature", {
     		console.log(frm.doc)p;
 	},
 });
+
+class Calculator {
+  constructor() {
+    this.currentValue = 0;
+    this.operator = null;
+    this.operand = null;
+  }
+
+  clear() {
+    this.currentValue = 0;
+    this.operator = null;
+    this.operand = null;
+  }
+
+  setOperand(value) {
+    this.operand = value;
+  }
+
+  setOperator(operator) {
+    if (this.operand !== null) {
+      this.evaluate();
+    }
+    this.operator = operator;
+    this.operand = this.currentValue;
+    this.currentValue = 0;
+  }
+
+  appendNumber(number) {
+    this.currentValue = this.currentValue * 10 + number;
+  }
+
+  evaluate() {
+    if (this.operator && this.operand !== null) {
+      switch (this.operator) {
+        case '+':
+          this.currentValue = this.operand + this.currentValue;
+          break;
+        case '-':
+          this.currentValue = this.operand - this.currentValue;
+          break;
+        case '*':
+          this.currentValue = this.operand * this.currentValue;
+          break;
+        case '/':
+          if (this.currentValue !== 0) {
+            this.currentValue = this.operand / this.currentValue;
+          } else {
+            throw new Error("Division by zero");
+          }
+          break;
+      }
+      this.operator = null;
+      this.operand = null;
+    }
+    return this.currentValue;
+  }
+}
