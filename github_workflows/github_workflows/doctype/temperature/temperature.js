0afc5a50797e52ba6de3359ed71c7f244995d911
Sambhav Saxena <sambhav.saxena@ambibuzz.com>
2025-02-15 10:09:03 +0530

    Update temperature.js

--- github_workflows/github_workflows/doctype/temperature/temperature.js
+++ github_workflows/github_workflows/doctype/temperature/temperature.js
@@ -1,8 +1,29 @@
 // Copyright (c) 2025, Ambibuzz Technologies LLP and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Temperature", {
-// 	refresh(frm) {
+frappe.ui.form.on("Temperature", {
+	refresh(frm) {
+    console.log(frm);
+	},
+});
 
-// 	},
-// });
+class Calculator {
+    add(a, b) {
+        return a + b;
+    }
+
+    subtract(a, b) {
+        return a - b;
+    }
+
+    multiply(a, b) {
+        return a * b;
+    }
+
+    divide(a, b) {
+        if (b === 0) {
+            throw new Error("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
