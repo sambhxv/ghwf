ee5a58dc4cee82c5175df0c8ec417e79edf4e4b2
Sambhav Saxena <sambhav.saxena@ambibuzz.com>
2025-02-17 17:11:36 +0530

    Update temperature.py

--- github_workflows/github_workflows/doctype/temperature/temperature.py
+++ github_workflows/github_workflows/doctype/temperature/temperature.py
@@ -20,5 +20,5 @@ class Calculator:
 
     def divide(self, a, b):
         if b == 0:
-            raise ValueError("Cannot divide by zero.")
+            raise ValueError("Cannot divide by zero")
         return a / b
