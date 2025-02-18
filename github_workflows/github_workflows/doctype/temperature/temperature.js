329373fc008c30c40339bc7107920acc0d15420d
Sambhav Saxena <sambhav.saxena@ambibuzz.com>
2025-02-18 12:12:00 +0530

    Update temperature.js

--- github_workflows/github_workflows/doctype/temperature/temperature.js
+++ github_workflows/github_workflows/doctype/temperature/temperature.js
@@ -23,7 +23,6 @@ class Calculator {
 
     divide(a, b) {
         if (b === 0) {
-			console.log("hehihihihihe");
             throw new Error("Cannot divide by zero!");
         }
         return a / b;
